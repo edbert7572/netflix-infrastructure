a71001af983303e16cba0086c5f2ec29f61042dd
edbert Nwokeji <edbert.nwokeji@gmail.com>
2022-09-28 21:59:32 -0500

    committing netflix infra modules

---
+++ ec2/variables.tf
@@ -0,0 +1,7 @@
+variable "ami_id" {
+  default = "ami-08e2d37b6a0129927"
+}
+
+variable "instance_type" {
+  default="t2.micro"
+}
---
+++ s3/variables.tf
@@ -0,0 +1,3 @@
+variable "bucket_name" {
+   default = "netflix-vamsi-978624"
+}
